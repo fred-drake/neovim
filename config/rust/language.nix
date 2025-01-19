ea5dcde53de272618bb9701566b629db7bcd9f14
Fred Drake <fred.drake@gmail.com>
2025-01-18 19:41:28 -0500

    Added directories to ignore for rust-analyzer

--- config/rust/language.nix
+++ config/rust/language.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   plugins = {
     conform-nvim = {
       enable = true;
-      settings = { formatters_by_ft.rust = [ "rustfmt" ]; };
+      settings = {formatters_by_ft.rust = ["rustfmt"];};
     };
     rustaceanvim = {
       enable = true;
@@ -14,15 +14,15 @@
         tools.enable_clippy = true;
         server = {
           default_settings = {
-            inlayHints = { lifetimeElisionHints = { enable = "always"; }; };
+            inlayHints = {lifetimeElisionHints = {enable = "always";};};
             rust-analyzer = {
-              cargo = { allFeatures = true; };
-              check = { command = "clippy"; };
+              cargo = {allFeatures = true;};
+              check = {command = "clippy";};
+              files = {excludeDirs = ["target" ".git" ".cargo" ".github" ".direnv"];};
             };
           };
         };
       };
     };
-
   };
 }

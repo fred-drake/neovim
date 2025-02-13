177a42454a143f0a459eb372e1401bbafd3bcd43
Fred Drake <fred.drake@gmail.com>
2025-02-12 20:16:29 -0500

    Updated python LSP

--- config/python/language.nix
+++ config/python/language.nix
@@ -2,25 +2,12 @@
   plugins = {
     conform-nvim = {
       enable = true;
-      settings = { formatters_by_ft.python = [ "black" ]; };
+      settings = {formatters_by_ft.python = ["black"];};
     };
-    lsp.servers.pylsp = {
+    # lsp.servers.pylsp.enable = false;
+    lsp.servers.pyright = {
       enable = true;
-      settings.plugins = {
-        black.enabled = true;
-        flake8.enabled = true;
-        isort.enabled = true;
-        jedi.enabled = true;
-        mccabe.enabled = true;
-        pycodestyle.enabled = true;
-        pydocstyle.enabled = true;
-        pyflakes.enabled = true;
-        pylint.enabled = true;
-        rope.enabled = true;
-        yapf.enabled = true;
-      };
     };
     none-ls.sources.formatting.black.enable = true;
   };
 }
-
